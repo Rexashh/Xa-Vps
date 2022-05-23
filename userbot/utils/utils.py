bbe814b3609c30b21464b9e7df9372623ee623f3
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-05-24 00:09:47 +0700

    Update utils.py

--- userbot/utils/utils.py
+++ userbot/utils/utils.py
@@ -10,7 +10,6 @@ import sys
 from pathlib import Path
 from random import randint
 
-import heroku3
 from telethon.tl.functions.contacts import UnblockRequest
 from telethon.tl.functions.channels import (
     CreateChannelRequest,
